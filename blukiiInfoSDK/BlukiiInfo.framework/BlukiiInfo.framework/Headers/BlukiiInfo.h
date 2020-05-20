4320388251475b9bde9a66191c22f989cd63688f
Lennart Fleig <lennart.fleig@schneiderpen.de>
2020-05-20 16:17:18 +0200

    Version 2.0.2
    - add support for more than now  Swift Version
    - bug fixes

---
+++ blukiiInfoSDK/BlukiiInfo.framework/BlukiiInfo.framework/Headers/BlukiiInfo.h
@@ -0,0 +1,48 @@
+//
+//  BlukiiInfo.h
+//  BlukiiInfo
+//
+//  Created by Lennart Fleig on 06.04.16.
+//  Copyright © 2016 Schneider Schreibgeräte GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BlukiiInfo.
+FOUNDATION_EXPORT double BlukiiInfoVersionNumber;
+
+//! Project version string for BlukiiInfo.
+FOUNDATION_EXPORT const unsigned char BlukiiInfoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlukiiInfo/PublicHeader.h>
+
+//#import "GTLMobileClient.h"
+#import "GTLRMobileclient.h"
+#import "GTLRAuth.h"
+#import "GTLRSecurebeacon.h"
+#import "InfoCrypto.h"
+//#import <CommonCrypto/CommonHMAC.h> // Wird nur für Amazaon Verschlüsselung benötigt
+
+//Google
+#import "GTLRBase64.h"
+#import "GTLRFramework.h"
+#import "GTLRRuntimeCommon.h"
+#import "GTLRURITemplate.h"
+#import "GTLRUtilities.h"
+#import "GTMMIMEDocument.h"
+#import "GTLRObject.h"
+#import "GTMSessionFetcher.h"
+#import "GTMSessionFetcherService.h"
+//#import "GTLRuntimeCommon.h"
+//#import "GTLTargetNamespace.h"
+//#import "GTMGatherInputStream.h"
+//#import "GTMHTTPFetchHistory.h"
+//#import "GTMHTTPFetcher.h"
+//#import "GTMHTTPFetcherLogging.h"
+//#import "GTMHTTPUploadFetcher.h"
+//#import "GTMOAuth2SignIn.h"
+//#import "GTMMIMEDocument.h"
+//#import "GTMReadMonitorInputStream.h"
+//#import "GTMOAuth2ViewControllerTouch.h"
+
+// #import "CocoaLumberjack/CocoaLumberjack.h"
